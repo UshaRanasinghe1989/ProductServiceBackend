6327535c1a1c0c0c2bd916019fb28118086e102f
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-11-17 12:18:34 +0530

    persist product API

--- src/main/java/com/icetteam/crm/product_service/controller/ProductController.java
+++ src/main/java/com/icetteam/crm/product_service/controller/ProductController.java
@@ -1,4 +1,17 @@
 package com.icetteam.crm.product_service.controller;
 
+import com.icetteam.crm.product_service.model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+    @PostMapping()
+    Product persist(@RequestBody Product product){
+        return null;
+    }
+
 }
